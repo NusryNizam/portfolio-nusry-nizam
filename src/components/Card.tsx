d2c3c0f7603f5b36d8b444e4ce617f9416634bc3
Nusry Nizam <nusrynizam@gmail.com>
2024-01-15 15:14:57 +0530

    added basic functionality to view portfolio items

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,94 @@
-import './Card.css'
+import "./Card.css";
 
-const Card = () => {
-    return (
-        <div className="card">
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+type CardProps = {
+  title: string;
+  description: string;
+};
+
+const Card = ({ title, description }: CardProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <div
+          className="card"
+          style={{ backgroundImage: `url(/images/onboarding.webp)` }}
+        >
+          <div className="card-info">
+            <h3 className="card-title">Recipe App</h3>
+            <p className="card-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              quibusdam pariatur obcaecati!
+            </p>
+          </div>
+          <div className="overlay"></div>
         </div>
-    )
-}
+      </SheetTrigger>
+
+      <SheetContent className="sheet" side={"bottom"}>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+
+          <SheetDescription className="sm:px-9 md:px-20 xl:px-40 ">
+            <Carousel className="carousel">
+              <CarouselContent className="carousel-content -ml-0">
+                <CarouselItem className="carousel-item sm:basis-1/1 md:basis-1/2 lg:basis-1/3">
+                  <img
+                    src="/images/onboarding.webp"
+                    className="carousel-image"
+                  />
+                </CarouselItem>
+                <CarouselItem className="carousel-item  sm:basis-1/1  md:basis-1/2 lg:basis-1/3">
+                  <img
+                    src="/images/onboarding.webp"
+                    className="carousel-image"
+                  />
+                </CarouselItem>
+                <CarouselItem className="carousel-item  sm:basis-1/1  md:basis-1/2 lg:basis-1/3">
+                  <img
+                    src="/images/onboarding.webp"
+                    className="carousel-image"
+                  />
+                </CarouselItem>
+                <CarouselItem className="carousel-item  sm:basis-1/1  md:basis-1/2 lg:basis-1/3">
+                  <img
+                    src="/images/onboarding.webp"
+                    className="carousel-image"
+                  />
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </SheetDescription>
+
+          <SheetFooter className="sheet-footer">
+            <SheetClose>
+              <button className="dismiss-btn">Dismiss</button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default Card;
--- src/sections/SectionTwo.tsx
+++ src/sections/SectionTwo.tsx
@@ -1,42 +1,19 @@
 import "./SectionTwo.css";
 import Card from "../components/Card.tsx";
 
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-
 const SectionTwo = () => {
   return (
     <section className="section-two" id="work">
       <h3 className="title">My Design Work</h3>
       <div className="card-container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          title={"New Game"}
+          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, mollitia."
+        />
+        <Card title={"New Game"} description="lorem23" />
+        <Card title={"New Game"} description="lorem23" />
+        <Card title={"New Game"} description="lorem23" />
       </div>
-      <Drawer>
-        <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent className="drawer">
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <button>Submit</button>
-            <DrawerClose>
-              <a href="#work">Cancel</a>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
     </section>
   );
 };
